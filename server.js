ebb7611f49078683d5e1b2f2fbfe0d85fc568201
Shameel Abdullah <shameel.abdullah@gmail.com>
2016-02-03 17:46:16 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var express = require('express'),
+    exphbs  = require('express-handlebars'); // "express-handlebars"
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+var data = {};
+
+app.get('/', function (req, res) {
+    res.render('home', {"data":data});
+});
+
+app.post('/', function(req,res){
+    //on post call
+    data = {"test":"test"};
+    res.send("posted");
+});
+
+app.listen(3000, function () {
+    console.log('express-handlebars example server listening on: 3000');
+});
