3cdb9c46282067a78db7c4194eb1190bd5b58ad5
Shameel Abdullah <shameel.abdullah@gmail.com>
2016-02-05 16:56:36 -0500

    prints round number in view

--- server.js
+++ server.js
@@ -27,13 +27,18 @@ app.get('/', function (req, res) {
 
 app.post('/', function(req,res){
     console.log('posted');
-    //console.log(req.body);
+    console.log(req.body);
     var payload = req.body;
     var id = payload.provider.steamid;
     if(shameelIds.indexOf(id) > -1){
         //shameel id
         //add game type, think its payload.map.mode
-        data.shameel.round = payload.map.round;
+        if(payload.map){
+            if(payload.map.round){                
+                console.log('setting data');
+                data.shameel.round = payload.map.round;
+            }
+        }
     }
     else if (nomarIds.indexOf(id) > -1){
         //nomar id
