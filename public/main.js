3c54a20b1dffe84c148276863a7d976f72c5018f
Shameel Abdullah <shameel.abdullah@gmail.com>
2016-02-17 17:30:16 -0500

    button re-enables after a minute

--- public/main.js
+++ public/main.js
@@ -6,20 +6,20 @@ var app = angular.module('myapp', ['btford.socket-io'])
         return socketFactory();
     });
 
-app.controller('mainController', function($scope, $http, socket) {
+app.controller('mainController', function($scope, $http, $timeout, socket) {
 
-    socket.on('data:shameel', function(data) {        
-        $scope.shameelPercentage = data.percentage;        
+    socket.on('data:shameel', function(data) {
+        $scope.shameelPercentage = data.percentage;
     });
 
-    socket.on('data:nomar', function(data){
+    socket.on('data:nomar', function(data) {
         $scope.nomarPercentage = data.percentage;
     });
 
-    socket.on('data:omas', function(data){
+    socket.on('data:omas', function(data) {
         $scope.omasPercentage = data.percentage;
     });
-    
+
     $scope.test = "test";
     $scope.shameelButtonText = "Request";
     $scope.shameelRequest = true;
@@ -28,26 +28,39 @@ app.controller('mainController', function($scope, $http, socket) {
     $scope.omasButtonText = "Request";
     $scope.omasRequest = true;
 
-    var enableButton = function(name){
-        $scope.shameelButtonText = "Request";
-        $scope.shameelRequest = true;
-    }
 
-    $scope.request = function(name) {        
+    $scope.request = function(name) {
 
         if (name == 'shameel') {
             $scope.shameelButtonText = "Requested";
             $scope.shameelRequest = false;
-            console.log($scope.shameelRequest);
-            $http.post('/request/shameel').then(function(res) {});            
+            $http.post('/request/shameel').then(function(res) {});
+            $timeout(function() {
+                console.log('test');
+                $scope.shameelButtonText = "Request";
+                $scope.shameelRequest = true;
+                console.log($scope.shameelRequest);
+            }, 60000);
         } else if (name == 'nomar') {
             $scope.nomarButtonText = "Requested";
             $scope.nomarRequest = false;
             $http.post('/request/nomar').then(function(res) {});
+            $timeout(function() {
+                console.log('test');
+                $scope.nomarButtonText = "Request";
+                $scope.nomarRequest = true;
+                console.log($scope.omasRequest);
+            }, 60000);
         } else {
             $scope.omasButtonText = "Requested";
             $scope.omasRequest = false;
             $http.post('/request/omas').then(function(res) {});
+            $timeout(function() {
+                console.log('test');
+                $scope.omasButtonText = "Request";
+                $scope.omasRequest = true;
+                console.log($scope.omasRequest);
+            }, 60000);
         }
     }
 
